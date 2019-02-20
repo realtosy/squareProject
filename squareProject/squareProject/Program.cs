20eab97cd243a51cc2533d14af4b16e3edc94b3f
realtosy <42302240+realtosy@users.noreply.github.com>
2019-02-20 12:22:41 +0300

    Add files via upload

---
+++ squareProject/squareProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squareProject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double a, b, h, r, S, pi = Math.PI;
+      string figureNumber;
+      Console.WriteLine("Площадь какой фигуры вы хотите посчитать? ");
+      Console.WriteLine("\n1. Треугольник\n2. Прямоугольник\n3. Круг");
+      figureNumber = Console.ReadLine();
+      switch (figureNumber)
+      {
+        case "1":
+          Console.WriteLine("\nВведите сторону и высоту треугольника\nСторона ");
+          a = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine("\nВысота ");
+          h = Convert.ToDouble(Console.ReadLine());
+          S = h * a * 0.5;
+          Console.WriteLine($"Площадь равна {S}");
+          break;
+        case "2":
+          Console.WriteLine("\nВведите стороны прямоугольника ");
+          a = Convert.ToDouble(Console.ReadLine());
+          b = Convert.ToDouble(Console.ReadLine());
+          S = b * a;
+          Console.WriteLine($"Площадь равна {S}");
+          break;
+        case "3":
+          Console.WriteLine("\nВведите радиус круга ");
+          r = Convert.ToDouble(Console.ReadLine());
+          S = Math.Round(pi * r * r, 2);
+          Console.WriteLine($"Площадь равна {S}");
+          break;
+        default:
+          Console.WriteLine("\nТакого пункта нет");
+          break;
+      }
+      Console.ReadKey(true);
+    }
+  }
+}
---
+++ squareProject/squareProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("squareProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("squareProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("deb69bc9-212e-43ee-a6ec-94c19be4829a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
